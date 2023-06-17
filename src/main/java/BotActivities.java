5bbbd7ced0e681bb6d05a54f9c04453d04af5f6a
Noor <noor@gmail.com>
2023-06-17 13:00:17 +0300

    Initial commit

---
+++ src/main/java/BotActivities.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BotActivities {
+
+    private int HowManyServicesDidHePerform ; //اجمال عدد الطلبات حتى الان
+    private int HowManyPeopleHaveTheRobotCommunicatedWith ; //عدد الناس الي تواصلو مع الروبوت للحين
+    private String MostActiveUsername ; //اكثر شخص نشط مع الروبوت
+    private int MostPopularActivityNumber ; //رقم النشاط الاكثر شعبيه
+
+    private List<String> activities = new ArrayList<>() ; //كل فعل بيسوي بينحفظ اسمه هني
+
+
+
+}
+
---
+++ src/main/java/Lead.java
@@ -0,0 +1,34 @@
+public class Lead {
+    private long chatId ;
+    private String name ;
+    private int supportStatus ;
+    private String phone;
+
+    public static final int SUPPORT_STATUS_UNKNOWN =0;
+
+    public static final int SUPPORT_STATUS_POSITIVE = 1 ;
+
+    public static final int SUPPORT_STATUS_NEGATIVE = 2 ;
+
+    public Lead (long chatId){
+        this.chatId = chatId ;
+        this.supportStatus = SUPPORT_STATUS_UNKNOWN ;
+
+    }
+
+    public void updateSupportStatus(String status){
+        if (status.equals("Y")){
+            this.supportStatus = SUPPORT_STATUS_POSITIVE ;
+        }else if (status.equals("N")){
+            this.supportStatus = SUPPORT_STATUS_NEGATIVE ;
+        }
+    }
+
+    public boolean isPositive(){
+        return this.supportStatus == SUPPORT_STATUS_POSITIVE ;
+    }
+
+    public void setPhone (String phone){
+        this.phone = phone ;
+    }
+}
